d2074bfda81f28fc00cf0db1c58de897d7257fa9
alfika <38674801+alfikamuis@users.noreply.github.com>
2022-08-27 14:41:24 +0700

    Delete src directory

--- src/main/java/com/learningRestfulSpringboot/SimpleCRUD/Config/DataConfig.java
+++
@@ -1,9 +0,0 @@
-package com.learningRestfulSpringboot.SimpleCRUD.Config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
-@Profile(value = "local")
-@Configuration
-public class DataConfig {
-}
--- src/main/java/com/learningRestfulSpringboot/SimpleCRUD/Repositories/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learningRestfulSpringboot.SimpleCRUD.Repositories;
-
-import com.learningRestfulSpringboot.SimpleCRUD.Entities.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book,Long> {
-}
