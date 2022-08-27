059210bc8b5beebe977e3e61c86d38f72622f007
AlfikaNM <alfikanmuis@gmail.com>
2022-08-27 23:14:44 +0700

    reupload

--- src/main/java/com/restfulSpringboot/JpaHibernate/service/StudentCardService.java
+++ src/main/java/com/restfulSpringboot/JpaHibernate/service/StudentCardService.java
@@ -1,8 +1,6 @@
 package com.restfulSpringboot.JpaHibernate.service;
 
-import com.restfulSpringboot.JpaHibernate.dto.BookDTO;
 import com.restfulSpringboot.JpaHibernate.dto.StudentCardDTO;
-import com.restfulSpringboot.JpaHibernate.model.Book;
 import com.restfulSpringboot.JpaHibernate.model.StudentCard;
 
 import java.util.List;
